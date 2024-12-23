a31358bd59603674d1c983e29d734e93049268a3
Viljar Nurmik <vuljas@c1r5p12.hive.fi>
2024-12-23 20:28:02 +0200

    Imroved

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -16,9 +16,11 @@ void  check_stack_order(t_stack *stack_a);
 void  sort_stack(t_stack *stack_a);
 void  sort_two(t_stack *stack_a);
 void  sort_three(t_stack *stack_a);
+void  sort_rest(t_stack *stack_a);
 void  sa(t_stack *stack_a);
 void  ra(t_stack *stack_a);
 void  rra(t_stack *stack_a);
+void  pb(t_stack *stack_a, t_stack *stack_b);
 
 int	main(int argc, char **argv)
 {
@@ -68,6 +70,8 @@ void  sort_stack(t_stack *stack_a)
     sort_two(stack_a);
   else if (stack_a->size == 3)
     sort_three(stack_a);
+  else
+    sort_rest(stack_a);
 }
 
 void  sort_two(t_stack *stack_a)
@@ -95,6 +99,28 @@ void  sort_three(t_stack *stack_a)
     rra(stack_a);
   else if (stack_a->arr[0] > stack_a->arr[1] && stack_a->arr[1] < stack_a->arr[2])
     ra(stack_a);
+}
+
+void  sort_rest(t_stack *stack_a)
+{
+  t_stack stack_b;
+
+  stack_b.arr = malloc(sizeof(int) * 1);
+  if (!stack_b.arr)
+  {
+    free(stack_a->arr);
+    exit(ft_printf("Error\n"));
+  }
+  ft_printf("HERE\n");
+  stack_b.size = 0;
+  while (stack_a->size > 3)
+  {
+    if (stack_a->arr[0] < stack_a->arr[1] && stack_a->arr[0] < stack_a->arr[stack_a->size - 1])
+    {
+      pb(stack_a, &stack_b);
+      stack_a->size--;
+    }
+  }
 }
 
 void  sa(t_stack *stack_a)
@@ -139,3 +165,46 @@ void  rra(t_stack *stack_a)
   ft_printf("rra\n");
 }
 
+void  pb(t_stack *stack_a, t_stack *stack_b)
+{
+  int temp;
+  int i;
+  int temp_arr[stack_b->size + 1];
+
+  temp = stack_a->arr[0];
+  i = 0;
+  while (i < stack_a->size - 1)
+  {
+    stack_a->arr[i] = stack_a->arr[i + 1];
+    i++;
+  }
+  stack_a->size--;
+  if (stack_b->size == 0)
+    stack_b->arr[0] = temp;
+  else
+  {
+    temp_arr[0] = temp;
+    i = 1;
+    while (i < stack_b->size)
+    {
+      temp_arr[i] = stack_b->arr[i - 1];
+      i++;
+    }
+    free(stack_b->arr);
+    stack_b->arr = malloc(sizeof(int) * stack_b->size + 1);
+    if (!stack_b->arr)
+		{
+			free(stack_b->arr);
+			ft_printf("Error filling stack_b!\n");
+			exit(-1);
+		}
+    i = 0;
+    while (i < stack_b->size)
+    {
+      stack_b->arr[i] = temp_arr[i];
+      i++;
+    }
+  }
+  stack_b->size++;
+  ft_printf("pb\n");
+}
