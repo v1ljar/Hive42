e5e6816c6dabd888c5084de68b378611e04d8857
Viljar Nurmik <vuljas@c1r5p6.hive.fi>
2025-01-14 17:05:59 +0200

    Cleaned

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,13 +6,11 @@
 /*   By: vuljas <vuljas@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 11:30:39 by vuljas            #+#    #+#             */
-/*   Updated: 2024/11/30 10:06:14 by vuljas           ###   ########.fr       */
+/*   Updated: 2024/12/05 12:18:37 by vuljas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "get_next_line.h"
-#include <fcntl.h>
 
 char	*ft_extract_line(char **buffer)
 {
@@ -20,8 +18,12 @@ char	*ft_extract_line(char **buffer)
 	char	*new_line;
 	char	*line;
 
-	if (*buffer[0] == '\0')
+	if (!*buffer || !**buffer)
+	{
+		free(*buffer);
+		*buffer = NULL;
 		return (NULL);
+	}
 	new_line = ft_strchr(*buffer, '\n');
 	if (new_line)
 	{
@@ -31,11 +33,10 @@ char	*ft_extract_line(char **buffer)
 	else
 	{
 		line = ft_strdup(*buffer);
-		new_buffer = ft_strdup("");
+		new_buffer = NULL;
 	}
 	free(*buffer);
-	*buffer = ft_strdup(new_buffer);
-	free(new_buffer);
+	*buffer = new_buffer;
 	return (line);
 }
 
@@ -45,41 +46,25 @@ char	*get_next_line(int fd)
 	char		temp[BUFFER_SIZE + 1];
 	int			bytes_read;
 
-	if (fd <= 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (!buffer)
 		buffer = ft_strdup("");
-	while (!ft_strchr(buffer, '\n'))
+	while (buffer && !ft_strchr(buffer, '\n'))
 	{
 		bytes_read = read(fd, temp, BUFFER_SIZE);
 		if (bytes_read < 0)
+		{
+			free(buffer);
+			buffer = NULL;
 			return (NULL);
+		}
 		if (bytes_read == 0)
 			break ;
 		temp[bytes_read] = '\0';
 		buffer = ft_strjoin(buffer, temp);
+		if (!buffer)
+			return (NULL);
 	}
 	return (ft_extract_line(&buffer));
-}
-
-int	main(void)
-{
-	int	fd;
-
-	char *str;
-
-	fd = open("test.txt", O_RDONLY);
-	if (fd < 0)
-	{
-		printf("Error!\n");
-		return (-1);
-	}
-
-	while ((str = get_next_line(fd)) != NULL)
-	{
-		printf("%s", str);
-		free(str);
-	}
-	close(fd);
-	return (0);
 }
