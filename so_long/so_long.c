1db964bb7ca21df831c57045584a93e41b8fba8a
Viljar Uljas <viljar1990@gmail.com>
2025-01-15 11:52:05 +0200

    Map validation

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -1,8 +1,10 @@
 #include "so_long.h"
 
 int     ft_validate_map(char *map_path, t_map_data *board);
-int     ft_all_to_big_str(char *map_path, t_map_data *board);
+int     ft_initialize_board(char *map_path, t_map_data *board);
 void    ft_initialize_map_data(t_map_data *board, char *whole_map, int lines);
+int     ft_validate_parts(t_map_data *board);
+int     ft_validate_amount(t_map_data *board);
 
 int main(int argc, char **argv)
 {
@@ -22,11 +24,79 @@ int main(int argc, char **argv)
 
 int ft_validate_map(char *map_path, t_map_data *board)
 {
-    ft_all_to_big_str(map_path, board);
+    if ((ft_initialize_board(map_path, board) == -1))
+        return (-1);
+    if ((ft_validate_parts(board) == -1))
+        return (-1);
+    if ((ft_validate_amount(board) == -1))
+        return (-1);
+    return (0);
+}
+int ft_validate_parts(t_map_data *board)
+{
+    int i;
+    int j;
+
+    i = 0;
+    while (i < board->rows)
+    {
+        j = 0;
+        while (board->map[i][j] != '\0')
+        {
+            if (board->map[i][j] == 'C')
+            {
+                board->collectibles++;
+                j++;
+            }
+            else if (board->map[i][j] == 'P')
+            {
+                board->start_count++;
+                j++;
+            }
+            else if (board->map[i][j] == 'E')
+            {
+                board->exit_count++;
+                j++;
+            }
+            else if (board->map[i][j] == '1' || board->map[i][j] == '0')
+                j++;
+            else
+                break;
+        }
+        if (j != board->line_len)
+            return (-1);
+        i++;
+    }
     return (0);
 }
 
-int ft_all_to_big_str(char *map_path, t_map_data *board)
+int     ft_validate_amount(t_map_data *b)
+{
+    int i;
+    int j;
+
+    if (b->exit_count != 1 || b->start_count != 1 || b->collectibles == 0)
+        return (-1);
+    i = 0;
+    while (i < b->rows)
+    {
+        j = 0;
+        while (b->map[i][j] != '\0')
+        {
+            if (b->map[0][j] != '1' || b->map[b->rows - 1][j] != '1')
+                return (-1);
+            if (b->map[i][0] != '1' || b->map[1][b->line_len - 1] != '1')
+                return (-1);
+            j++;
+        }
+        i++;
+    }
+    ft_printf("Exits: %i\tStarts: %i\tCollectibles: %i\tRows: %i\tLine len:%i\n", b->exit_count, b->start_count, b->collectibles, b->rows, b->line_len);
+
+    return (0);
+}
+
+int ft_initialize_board(char *map_path, t_map_data *board)
 {
     char    *whole_map;
     int     fd;
@@ -43,6 +113,11 @@ int ft_all_to_big_str(char *map_path, t_map_data *board)
         lines++;
     }
     close(fd);
+    if (lines < 3 || (ft_strlen(whole_map) < 17))
+    {
+        free(whole_map);
+        return (-1);
+    }
     ft_initialize_map_data(board, whole_map, lines);
     return (0);
 }
