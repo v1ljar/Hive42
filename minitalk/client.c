84133abeae2b94af0b0dfb4089d75b7449b6a76a
Viljar Nurmik <vuljas@c1r4p7.hive.fi>
2025-02-14 17:24:41 +0200

    Improving

--- minitalk/client.c
+++ minitalk/client.c
@@ -1,7 +1,23 @@
 #include "./libft/libft.h"
+#include <signal.h>
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
-	ft_printf("Helllo World\n");
+	int		pid;
+	char	*str;
+	int		sigset;
+	sigset_t	set;
+
+	if (argc != 3)
+		exit(ft_printf("Client program requires 2 arguments! PID and message string\n"));
+	pid = ft_atoi(argv[1]);
+	str = argv[2];
+	sigset = sigemptyset(&set);
+	sigaddset(&set, SIGUSR1);
+	ft_printf("PID: %i\nSTRING: %s\nSIGSET: %i\n", pid, str, sigset);
+	ft_printf("1 if SIGUSR1 is member: %i\n", sigismember(&set, SIGUSR1));
+	kill(pid, SIGUSR1);
+	write(pid, &str, ft_strlen(str));
+	ft_printf("Sent a signal\n");
 	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -3,6 +3,16 @@
 
 int	main(void)
 {
+	int		sigset;
+	sigset_t	set;
+
 	ft_printf("PID: %i", getpid());
+	sigset = sigemptyset(&set);
+	sigaddset(&set, SIGUSR1);
+	sigaction(SIGUSR1, NULL, NULL);
+	while (1)
+	{
+
+	}
 	return (0);
 }
