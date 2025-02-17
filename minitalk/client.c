c618cf053e0d88854264543598e3362b07c21439
Viljar Uljas <viljar1990@gmail.com>
2025-02-17 11:38:21 +0200

    Something

--- minitalk/client.c
+++ minitalk/client.c
@@ -1,23 +1,49 @@
 #include "./libft/libft.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include <signal.h>
 
-int	main(int argc, char *argv[])
-{
-	int		pid;
-	char	*str;
-	int		sigset;
-	sigset_t	set;
 
-	if (argc != 3)
-		exit(ft_printf("Client program requires 2 arguments! PID and message string\n"));
-	pid = ft_atoi(argv[1]);
-	str = argv[2];
-	sigset = sigemptyset(&set);
-	sigaddset(&set, SIGUSR1);
-	ft_printf("PID: %i\nSTRING: %s\nSIGSET: %i\n", pid, str, sigset);
-	ft_printf("1 if SIGUSR1 is member: %i\n", sigismember(&set, SIGUSR1));
-	kill(pid, SIGUSR1);
-	write(pid, &str, ft_strlen(str));
-	ft_printf("Sent a signal\n");
-	return (0);
+void send_bits(int pid, char *str)
+{
+    int i;
+    unsigned char octet;
+
+    while (*str)
+    {
+        i = 8;
+        octet = *str++;
+        while (i--)
+        {
+            if (octet >> i & 1)
+            {
+                kill(pid, SIGUSR1);
+            }
+            else
+            {
+                kill(pid, SIGUSR2);
+            }
+            usleep(500);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Usage: %s <PID> <0 or 1>\n", argv[0]);
+        return 1;
+    }
+
+    int sig = atoi(argv[1]);
+    if (sig <= 0)
+    {
+        printf("Invalid PID.\n");
+        return 1;
+    }
+
+    send_bits(sig, argv[2]);
+    printf("Signal sent to PID %d\n", sig);
+    return 0;
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -1,18 +1,56 @@
 #include "./libft/libft.h"
 #include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int	main(void)
+void bit_write(int x)
 {
-	int		sigset;
-	sigset_t	set;
+    static int bit = 7;
+    static unsigned char set = 0;
 
-	ft_printf("PID: %i", getpid());
-	sigset = sigemptyset(&set);
-	sigaddset(&set, SIGUSR1);
-	sigaction(SIGUSR1, NULL, NULL);
-	while (1)
-	{
+    set += (x << bit);
 
-	}
-	return (0);
+    if (bit == 0)
+    {
+        write(1, &set, 1);
+        set = 0;
+        bit = 7;
+    }
+    else
+    {
+        bit--;
+    }
+}
+void handle_signal(int signum, siginfo_t *info, void *context)
+{
+    if(signum == SIGUSR1)
+        bit_write(1);
+    else
+        bit_write(0);
+    (void)info;
+    (void)context;
+}
+
+int main()
+{
+    struct sigaction sa;
+    
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = handle_signal;
+    sigemptyset(&sa.sa_mask);
+
+    if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+    {
+        perror("sigaction failed");
+        return 1;
+    }
+
+    printf("Server running. PID: %d\n", getpid());
+
+    while (1)
+    {
+        pause();
+    }
+
+    return 0;
 }
