303abb6e0e181300a9baeb19f5328b2392811232
v1ljar <viljar1990@gmail.com>
2025-10-16 15:41:06 +0300

    Improved ex01

--- CPP00/ex01/phonebook.hpp
+++ CPP00/ex01/phonebook.hpp
@@ -4,13 +4,14 @@
 #include <string>
 
 class Contact{
-	public:
+	private:
 		std::string	first_name;
 		std::string	last_name;
 		std::string	nickname;
-	private:
 		std::string	phone_number;
 		std::string	darkest_secret;
+	public:
+		
 };
 
 class PhoneBook{
