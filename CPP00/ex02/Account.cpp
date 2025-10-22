d6c7bd3bfe76b0a5086aa35a614270f6e8430ea5
Viljar Uljas <viljar1990@gmail.com>
2025-10-22 23:58:05 +0300

    CPP00ex02 improving

---
+++ CPP00/ex02/Account.cpp
@@ -0,0 +1,22 @@
+#include "Account.hpp"
+
+int	Account::getNbAccounts( void )
+{
+	return (0);
+}
+int	Account::getTotalAmount( void )
+{
+
+}
+int	Account::getNbDeposits( void )
+{
+
+}
+int	Account::getNbWithdrawals( void )
+{
+
+}
+void	Account::displayAccountsInfos( void )
+{
+
+}
