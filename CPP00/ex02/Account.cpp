0227670516ad8ce670caf6b4b644a99cd41f5724
v1ljar <viljar1990@gmail.com>
2025-10-23 12:09:15 +0300

    CPP00ex02 improving

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -1,22 +1,60 @@
 #include "Account.hpp"
+#include <iostream>
+
+int	index = 0;
+
+Account::Account(int initial_deposit) : _nbDeposits(0), _nbWithdrawals(0) {
+	_amount = initial_deposit;
+	_accountIndex = index++;
+	std::cout 	<< "[Timestamp here] " << "index:" << _accountIndex
+				<< ";amount:" << _amount << ";created" << std::endl;
+}
+
+Account::~Account(void) {}
 
 int	Account::getNbAccounts( void )
 {
-	return (0);
+	return (index + 1);
 }
 int	Account::getTotalAmount( void )
 {
-
+	return (0);
 }
 int	Account::getNbDeposits( void )
 {
-
+	return (0);
 }
 int	Account::getNbWithdrawals( void )
 {
-
+	return (0);
 }
 void	Account::displayAccountsInfos( void )
+{
+
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	(void)deposit;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	(void)withdrawal;
+	return (false);
+}
+
+int		Account::checkAmount( void ) const
+{
+	return (0);
+}
+
+void	Account::displayStatus( void ) const
+{
+
+}
+
+void	Account::_displayTimestamp( void )
 {
 
 }
