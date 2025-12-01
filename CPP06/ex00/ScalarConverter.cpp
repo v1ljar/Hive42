5eed90b01b73a5c69d0e491a791a2a8e11707651
Viljar Uljas <viljar1990@gmail.com>
2025-12-01 15:23:48 +0200

    CPP06ex00 almost

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -11,10 +11,10 @@ void ScalarConverter::convert(const std::string& literal)
 		convertChar(literal, &c_res, &i_res, &d_res, &f_res);
 	else if (isInt(literal))
 		convertInt(literal, &c_res, &i_res, &d_res, &f_res);
-	else if (isDouble(literal))
-		convertDouble(literal, &c_res, &i_res, &d_res, &f_res);
 	else if (isFloat(literal))
 		convertFloat(literal, &c_res, &i_res, &d_res, &f_res);
+	else if (isDouble(literal))
+		convertDouble(literal, &c_res, &i_res, &d_res, &f_res);
 	else
 		throw NotValidLiteralException();
 	std::cout << "char: " << c_res
@@ -31,65 +31,39 @@ const char* NotValidLiteralException::what() const noexcept
 
 bool isChar(const std::string& literal)
 {
-	return (literal.size() == 1 && !std::isdigit(literal[0]));
+	return (literal.size() == 1 && std::isprint(literal[0]) && !std::isdigit(literal[0]));
 }
 
 bool isInt(const std::string& literal)
 {
-	size_t i = 0;
-
-	if (literal[i] == '-' || literal[i] == '+')
-		i++;
-	for (; i < literal.length(); i++)
-	{
-		if (!std::isdigit(literal[i]))
-			return (false);
-	}
-	return (true);
-}
-
-bool isDouble(const std::string& literal)
-{
-	size_t	i = 0;
-	bool	dot_found = false;
-
-	if (literal[i] == '-' || literal[i] == '+')
-		i++;
-	for (; i < literal.length(); i++)
-	{
-		if (literal[i] == '.')
-		{
-			if (dot_found)
-				return (false);
-			dot_found = true;
-		}
-		else if (!std::isdigit(literal[i]))
-			return (false);
-	}
-	return (dot_found);
+	char *end;
+	long nbr = std::strtol(literal.c_str(), &end, 10);
+	if (*end != '\0')
+		return (false);
+	return (nbr >= std::numeric_limits<int>::min() && nbr <= std::numeric_limits<int>::max());
 }
 
 bool isFloat(const std::string& literal)
 {
-	size_t	i = 0;
-	bool	dot_found = false;
-
-	if (literal[i] == '-' || literal[i] == '+')
-		i++;
-	for (;i < literal.length(); i++)
-	{
-		if (literal[i] == '.')
-		{
-			if (dot_found)
-				return (false);
-			dot_found = true;
-		}
-		else if(!std::isdigit(literal[i]))
-			break;
-	}
-	if (i == literal.length() - 1 && literal[i] == 'f')
+	if (literal == "-nanf" || literal == "-inff" || literal == "+inff")
 		return (true);
-	return (false);
+
+	size_t	end;
+	float	nbr = std::stof(literal, &end);
+	if (literal[end] != 'f' || end != literal.length() - 1)
+		return (false);
+	return (nbr >= std::numeric_limits<float>::min() && nbr <= std::numeric_limits<float>::max());
+}
+
+bool isDouble(const std::string& literal)
+{
+	if (literal == "-nan" || literal == "-inf" || literal == "+inf")
+		return (true);
+	char*	end;
+	double	nbr = std::strtod(literal.c_str(), &end);
+	if (*end != '\0')
+		return (false);
+	return (nbr >= std::numeric_limits<double>::min() && nbr <= std::numeric_limits<double>::max());
 }
 
 void convertChar(const std::string& literal, char *c_res, int *i_res, double *d_res, float *f_res)
@@ -122,6 +96,8 @@ void convertDouble(const std::string& literal, char *c_res, int *i_res, double *
 void convertFloat(const std::string& literal, char *c_res, int *i_res, double *d_res, float *f_res)
 {
 	std::cout << "FLOAT [ " << literal << " ]\n";
+	if (literal == "-nan" || literal == "-inf" || literal == "+inf")
+		return ;
 	*f_res = std::stof(literal, nullptr);
 	*i_res = static_cast<int>(*f_res);
 	*c_res = static_cast<char>(*f_res);
