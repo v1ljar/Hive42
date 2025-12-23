3e192957503dc4e5da933d8e8e632b1d7d781ccf
v1ljar <viljar1990@gmail.com>
2025-12-23 12:52:34 +0200

    CPP07 added test cases for ex02

--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -5,6 +5,7 @@
 int main(int, char**)
 {
     Array<int> numbers(MAX_VAL);
+    std::cout << "Size of the numbers array is: " << numbers.size() << "\n";
     int* mirror = new int[MAX_VAL];
     srand(time(NULL));
     for (int i = 0; i < MAX_VAL; i++)
@@ -13,10 +14,25 @@ int main(int, char**)
         numbers[i] = value;
         mirror[i] = value;
     }
+    try
+    {
+        std::cout << "Value of numbers[-10]:\t" << numbers[-10] << "\n";
+
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << "Value of numbers[10]:\t" << numbers[10] << "\n";
+    std::cout << "Value of mirror[10]:\t" << mirror[10] << "\n";
     //SCOPE
     {
         Array<int> tmp = numbers;
+        std::cout << "Size of the tmp array is: " << tmp.size()
+                  << ". Value of tmp[10]: " << tmp[10] << "\n";
         Array<int> test(tmp);
+        std::cout << "Size of the test array is: " << test.size()
+                  << ". Value of test[10]: " << test[10] << "\n";
     }
     for (int i = 0; i < MAX_VAL; i++)
     {
