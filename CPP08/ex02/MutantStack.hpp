35f0c666ac460c30d049063ce5d680450cb1fd39
Viljar Uljas <viljar1990@gmail.com>
2026-01-02 14:23:16 +0200

    CPP08ex02 improved

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -1,21 +1,72 @@
 #pragma once
 
 #include <stack>
-#include <iostream>
+#include <deque>
+#include <iterator>
+#include <cstddef>
 
 template <typename T>
 class MutantStack : public std::stack<T> {
 	public:
 		MutantStack() = default;
 		MutantStack(const MutantStack& other) {
-			return *this;
+			*this = other;
 		};
 		MutantStack& operator=(const MutantStack& other) {
 			if (this != &other) {
-				
+				std::stack<T>::operator=(other);
 			}
+			return *this;
 		};
 		~MutantStack() = default;
 
-		// Member functions
+		class iterator {
+			public:
+				using iterator_category = std::bidirectional_iterator_tag;
+				using value_type = T;
+				using difference_type = std::ptrdiff_t;
+				using pointer = T*;
+				using reference = T&;
+
+				explicit iterator(typename std::deque<T>::iterator it) : _it(it) {}
+
+				reference operator*() const { return *_it; }
+				pointer operator->() { return &(*_it); }
+
+				iterator& operator++() {
+					++_it;
+					return *this;
+				}
+
+				iterator operator++(int) {
+					iterator tmp = *this;
+					++_it;
+					return tmp;
+				}
+
+				iterator& operator--() {
+					--_it;
+					return *this;
+				}
+
+				iterator operator--(int) {
+					iterator tmp = *this;
+					--_it;
+					return tmp;
+				}
+
+				bool operator==(const iterator& other) const { return _it == other._it; }
+				bool operator!=(const iterator& other) const { return _it != other._it; }
+
+			private:
+				typename std::deque<T>::iterator _it;
+		};
+
+		iterator begin() {
+			return iterator(this->c.begin());
+		}
+
+		iterator end() {
+			return iterator(this->c.end());
+		}
 };
--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -1,8 +1,11 @@
 
 #include "MutantStack.hpp"
+#include <iostream>
+#include <list>
 
 int main()
 {
+	std::cout << "\n--- [ Test1: test from the subject ] ---\n";
 	MutantStack<int> mstack;
 
 	mstack.push(5);
@@ -20,16 +23,45 @@ int main()
 	//[...]
 	mstack.push(0);
 
-	// MutantStack<int>::iterator it = mstack.begin();
-	// MutantStack<int>::iterator ite = mstack.end();
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
 
-	// ++it;
-	// --it;
-	// while (it != ite)
-	// {
-	// 	std::cout << *it << std::endl;
-	// 	++it;
-	// }
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
 	std::stack<int> s(mstack);
+
+	std::cout << "\n--- [ Test2: test with list ] ---\n";
+	std::list<int> mstack2;
+
+	mstack2.push_back(5);
+	mstack2.push_back(17);
+
+	std::cout << mstack2.front() << std::endl;
+
+	mstack2.pop_back();
+
+	std::cout << mstack2.size() << std::endl;
+
+	mstack2.push_back(3);
+	mstack2.push_back(5);
+	mstack2.push_back(737);
+	//[...]
+	mstack2.push_back(0);
+	std::list<int>::iterator it2 = mstack2.begin();
+	std::list<int>::iterator ite2 = mstack2.end();
+
+	++it2;
+	--it2;
+	while (it2 != ite2)
+	{
+		std::cout << *it2 << std::endl;
+		++it2;
+	}
+
 	return 0;
 }
