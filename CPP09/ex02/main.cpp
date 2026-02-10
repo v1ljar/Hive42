4cfe234f7bc021fe60667eb32cb32b2797cbde84
v1ljar <viljar1990@gmail.com>
2026-02-10 10:56:14 +0200

    CPP09ex02 improved

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -1,6 +1,13 @@
 #include "PmergeMe.hpp"
 
 int main(int ac, char **av) {
-    
+    if (ac == 1) {
+        std::cerr << "Program requires a positive integer sequence as an argument!\n";
+        return (1);
+    }
+    for (int i = 1; i < ac; i++) {
+        std::cout << av[i] << " ";
+    }
+    std::cout << "\n";
     return (0);
 }
